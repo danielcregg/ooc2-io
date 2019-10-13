461bab31f98ef98b253d905474ecb804db0fe1cf
danielcregg <daniel.cregg@gmail.com>
2019-10-13 14:04:31 +0100

    Cleaned up code

--- src/ie/gmit/ooc2labs/io/FileIOTest.java
+++ src/ie/gmit/ooc2labs/io/FileIOTest.java
@@ -1,21 +1,25 @@
 package ie.gmit.ooc2labs.io;
 
+import java.io.File;
 import java.util.Scanner;
 import java.util.Formatter;
-import java.io.File;
 import java.io.IOException;
-   
-public class FileIOTest {  // saved as "FileIOTest.java"
-   public static void main (String [] args) throws IOException {
-      Scanner in = new Scanner(new File(".\\resources\\in.txt"));        // file input
-      Formatter out = new Formatter(new File(".\\resources\\out.txt"));  // file output
-   
-      int a = in.nextInt();
-      int b = in.nextInt();
-      out.format("%d\n",a+b);  // format() has the same syntax as printf()
-      System.out.println("Output have been created...");
-      
-      in.close();	// Close the scanner object	
-      out.close();    // flush the output and close the output file
-   }
+
+public class FileIOTest { // saved as "FileIOTest.java"
+	public static void main(String[] args) throws IOException {
+		File inputFile = new File(".\\resources\\in.txt");
+		File outputFile = new File(".\\resources\\out.txt");
+		Scanner inputScanner = new Scanner(inputFile);
+		Formatter outputFormatted = new Formatter(outputFile);
+		
+		int firstInt = inputScanner.nextInt();
+		int secondInt = inputScanner.nextInt();
+		// format() has the same syntax as printf()
+		outputFormatted.format("%d\n", firstInt + secondInt); 
+		
+		System.out.println("Output has been formatted and sent to output.txt");
+
+		inputScanner.close(); // Close the scanner object
+		outputFormatted.close(); // flush the output and close the output file
+	}
 }
