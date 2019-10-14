25b0d6a72d391e7e4aae569f3de572acf866eb4b
danielcregg <daniel.cregg@gmail.com>
2019-10-14 19:45:39 +0100

    Cleaned up code

--- src/ie/gmit/ooc2labs/io/CopyFileChar.java
+++ src/ie/gmit/ooc2labs/io/CopyFileChar.java
@@ -1,24 +1,32 @@
 package ie.gmit.ooc2labs.io;
 
-import java.io.*;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 public class CopyFileChar {
 
 	public static void main(String args[]) throws IOException {
 		FileReader in = null;
 		FileWriter out = null;
+		int c;
 
 		try {
 			in = new FileReader("resources/input.txt");
 			out = new FileWriter("resources/output.txt");
-			int c;
 
 			while ((c = in.read()) != -1) {
 				// Uncomment to see bytes copied - http://www.asciitable.com/
-				//System.out.println(c);
+				// System.out.println(c);
 				System.out.printf("%d - %c\n", c, c);
+				// Sleep for half a second (500 milliseconds)
+				TimeUnit.MILLISECONDS.sleep(500);
 				out.write(c);
 			}
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		} finally {
 			if (in != null) {
 				in.close();
